85f861a3cae6a3ab09ed878b3797b731236cc058
ChandanaShivakumar <chandanashiva55@gmail.com>
2023-06-09 09:20:18 +0530

    Changed the jsonserver access link

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -47,7 +47,7 @@ export class LoginComponent {
 
   logindata(loginForm: FormGroup){
     //console.log(this.loginForm.value);
-    this._http.get<any>("http://localhost:3000/users")
+    this._http.get<any>("https://angular-jsonserver.vercel.app/users")
     .subscribe(res =>{
       const user = res.find((a: any)=>{
         return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
